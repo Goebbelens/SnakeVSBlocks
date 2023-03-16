59fe0007c3eea2b41165ea9446addd7e1d368625
Goebbelens <doronin_ivan@mail.ru>
2023-03-16 17:41:30 +0300

    Controls fully functional

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -18,5 +18,23 @@ public class PlayerControls : MonoBehaviour
             Player.position = new Vector3(Player.position.x + delta.x * MovementVelocity, 0, Player.position.z + delta.y * MovementVelocity);
         }
         _previousMousePosition = Input.mousePosition;
+
+        if (Player.position.z > 10.87999)
+        {
+            Player.position = new Vector3 (Player.position.x, 0, (float)10.87999);
+        }
+        if (Player.position.z < -10.87999)
+        {
+            Player.position = new Vector3(Player.position.x, 0, (float)-10.87999);
+        }
+
+        if (Player.position.x > 7.239995)
+        {
+            Player.position = new Vector3((float)7.239995, 0, Player.position.z);
+        }
+        if (Player.position.x < -7.239995)
+        {
+            Player.position = new Vector3((float)-7.239995, 0, Player.position.z);
+        }
     }
 }
