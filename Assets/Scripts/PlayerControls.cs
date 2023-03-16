e576f5c3593a10ee20d4e2761236c7dd43766ebe
Goebbelens <doronin_ivan@mail.ru>
2023-03-16 17:18:41 +0300

    Refined controls

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -7,15 +7,15 @@ public class PlayerControls : MonoBehaviour
 {
     public Transform Player;
     private Vector3 _previousMousePosition;
-    public float MovementVelocity = 1.0f;
+    public float MovementVelocity = 0.04f;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
+            //Vector3 playerPos = Player.position;
             Vector3 delta = Input.mousePosition - _previousMousePosition;
-            Player.transform.position = new Vector3(delta.x * MovementVelocity, delta.y * MovementVelocity, 0);
+            Player.position = new Vector3(Player.position.x + delta.x * MovementVelocity, 0, Player.position.z + delta.y * MovementVelocity);
         }
         _previousMousePosition = Input.mousePosition;
     }
