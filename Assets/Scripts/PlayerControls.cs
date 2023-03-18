2ad6f966e197b5065c88af38e7ad2b771ddce536
Goebbelens <doronin_ivan@mail.ru>
2023-03-18 13:00:49 +0300

    Clipping > Controls

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    public Rigidbody SphereRigidBody;
+    public float SphereXSpeed = 1.0f;
+    public float SphereYSpeed = 1.0f;
+
     public Transform Player;
     public Transform ZoneCenter;
     private Vector3 _previousMousePosition;
     public float MovementVelocity = 0.012f;
 
-    void Update()
+    private void Awake()
+    {
+        //SphereRigidBody.velocity = new Vector3 (SphereXSpeed, SphereYSpeed, 0);
+    }
+
+    // Changed from Update to FixedUpdate
+    void FixedUpdate()
     {
         if (Input.GetMouseButton(0))
         {
             //Vector3 playerPos = Player.position;
+            //SphereRigidBody.velocity = new Vector3(SphereXSpeed, SphereYSpeed, 0);
             Vector3 delta = Input.mousePosition - _previousMousePosition;
-            Player.localPosition = new Vector3(Player.localPosition.x + delta.x * MovementVelocity, Player.localPosition.y + delta.y * MovementVelocity, 0);
+            SphereRigidBody.AddForce(new Vector3(delta.x * 0.02f, delta.y * 0.02f, 0), ForceMode.Impulse);
+            //Player.localPosition = new Vector3(Player.localPosition.x + delta.x * MovementVelocity, Player.localPosition.y, 0);
         }
         _previousMousePosition = Input.mousePosition;
 
